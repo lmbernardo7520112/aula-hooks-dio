dfcbc188112c462746503133837aa5b93c9329f7
lmbernardo7520112 <lmbernardo752011@gmail.com>
2022-03-18 09:56:00 -0300

    Concluded lesson useState/useEffect ifoodcounter

---
+++ src/Components/App/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+//import Counter from '../Counter/Counter';
+//import SmartCounter from '../Smartcounter/Smartcounter';
+import IfoodCounter from '../IfoodCounter/IfoodCounter';
+
+function App() {
+  return (
+    <>
+    <h1> Hello</h1>
+    <IfoodCounter />
+    </>
+  );
+}
+
+export default App;
